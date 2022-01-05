897f62b399e7ce36692e234dc22e13cb2eeae59d
FranciscoMaik <chiconunes3081@gmail.com>
2022-01-05 14:44:57 -0300

    feat: create application

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactSpeedometer,{ Transition } from 'react-d3-speedometer';
 
 function App() {
+  const [contage, setContage] = useState([
+    {maxValue: 1000, minValue: 0, value: 300, label: 'Sharpe'},
+    {maxValue: 400, minValue: 0, value: 300, label: 'Volatilidade'},
+    {maxValue: 100, minValue: 0, value: 50, label: 'Previsibilidade'},
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='center'>
+        {contage.map((item,index) => (
+          <ReactSpeedometer 
+            key={index}
+            minValue={item.minValue}
+            maxValue={item.maxValue}
+            value={item.value}
+            startColor='#ff5252'
+            endColor='#52eb7d'
+            width={250}
+            height={250}
+            ringWidth={24}
+            maxSegmentLabels={0}
+            segments={1000}
+            needleHeightRatio={0.6}
+            needleColor='#ffffff'
+            needleTransition={Transition.easeElasticOut}
+            needleTransitionDuration={5000}
+            textColor='#ffffff'
+            forceRender
+            currentValueText={`${item.label}: ${item.value}`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
